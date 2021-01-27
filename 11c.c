afd3332ad1432a44032560658e5e6553c5e02cba
Kahuka0506 <kou22uhneiom@icloud.com>
2021-01-27 19:11:28 +0900

    11c

--- 11c.c
+++ 11c.c
@@ -8,6 +8,7 @@
 
 
 
+char *user_input;
 
 typedef enum{
     TK_RESERVED,
@@ -20,6 +21,7 @@ struct Token{
     Token *next;
     int val;
     char *str;
+    int len;
 };
 Token *token;
 
@@ -44,7 +46,7 @@ struct Node{
 
 
 
-char *user_input;
+
 
 
 void error(char *format, ...){
@@ -75,8 +77,8 @@ void error_at(char *loc, char *format, ...){
 
 
 
-bool consume(char op){
-    if(token->kind != TK_RESERVED || token->str[0] != op) return false;
+bool consume(char *op){
+    if(token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len)) return false;
     token = token->next;
     return true;
 }
@@ -84,7 +86,7 @@ bool consume(char op){
 
 
 void expect(char op){
-    if(token->kind != TK_RESERVED || token->str[0] != op) {
+    if(token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len)) {
         //error("not '%c'", op);
         error_at(token->str, "not '%c'", op);
     }
@@ -130,7 +132,7 @@ Token *tokenize(char *p){
     while (*p) {
         if(isspace(*p)){p++; continue;}
 
-        if(*p == '+' || *p == '-'){
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
             cur = new_token(TK_RESERVED, cur, p);
             p++;
             continue;
@@ -159,6 +161,121 @@ Token *tokenize(char *p){
 
 
 
+Node *primary();
+Node *mul();
+Node *expr();
+Node *unary();
+Node *equality();
+Node *relational();
+Node *add();
+
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary(){
+
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+Node *mul(){
+    Node *node = unary();
+
+    for(;;){
+        if(consume('*')){
+            node = new_node(ND_MUL, node, unary());
+        }else if(consume('/')){
+            node = new_node(ND_DIV, node, unary());
+        }else{
+            return node;
+        }
+    }
+}
+
+Node *expr(){
+    Node *node = mul();
+
+    for (;;) {
+        if(consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }else if(consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }else {
+            return node;
+        }
+    }
+}
+
+Node *unary(){
+    if(consume('+')){
+        return primary();
+    }
+    if(consume('-')){
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+} 
+
+
+
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char *argv[]){
 
@@ -171,26 +288,19 @@ int main(int argc, char *argv[]){
 
     user_input = argv[1];
     token = tokenize(argv[1]);
-
+    Node *node = expr();
 
 
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", expect_number());
 
 
-    while (!at_eof()) {
-        if(consume('+')){
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
+    gen(node);
 
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
 
+    printf("  pop rax\n");
     printf("  ret\n");
 
     return 0;
